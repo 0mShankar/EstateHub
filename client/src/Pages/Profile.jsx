96d484f2c4d3d6174ebe16a8a787238045e4ba58
0mShankar <omshankar9798@gmail.com>
2023-11-17 12:48:32 +0530

    complete your user listings functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -23,6 +23,8 @@ const [filePerc, setFilePerc]=useState(0);
 const [fileUploadError, setFileUploadError] =useState(false);
 const [formData, setFormData]=useState({});
 const [updateSuccess, setUpdateSuccess]=useState(false);
+const [showListingsError, setShowListingsError]=useState(false);
+const [userListings, setUserListings] =useState([]); 
 const dispatch=useDispatch();
 
 
@@ -119,7 +121,21 @@ const handleSignOut=async ()=>{
     dispatch(deleteUserFailure(data.message));
   }
 };
+const handleShowListings =async()=>{
+  try {
+    setShowListingsError(false);
+    const res =await fetch(`/api/user/listings/${currentUser._id}`);
+    const data =await res.json();
 
+    if(data.success === false){
+      setShowListingsError(true);
+      return;
+    }
+    setUserListings(data);
+  } catch (error) {
+    setShowListingsError(true);
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -147,7 +163,31 @@ const handleSignOut=async ()=>{
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
+        <button onClick={handleShowListings} className='text bg-green-700 w-full rounded-lg'>Show Listings</button>
+             <p className='text-red-700 mt-5'>{showListingsError ?'Error showing listings':''}</p>
+
+
+
+             {userListings && userListings.length >0 &&
+             <div className="flex flex-col gap-4 ">
+              <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+              {userListings.map((listing)=>
+                <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'> 
+                    <Link className='flex-1  text-slate-700 font-semibold  hover:underline truncate' to={`/listing/${listing._id}`}>
+                      <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain '/>
+                    </Link>
+                    <Link to={`/listing/${listing._id}`}><p >{listing.name}</p></Link>
+
+                    <div className="flex flex-col">
+                        <button className='text-red-700 uppercase'>Delete</button>
+                        <button className='text-green-700 uppercase'>Edit</button>
+                    </div>
+                  </div>
+              )}
+             
+             </div>}
         <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign-out</span>
+      
       </div>
 
       
