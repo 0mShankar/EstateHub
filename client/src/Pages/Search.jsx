4ec5ac7aa8a79ef66dbeccab7c013da233c01064
0mShankar <omshankar9798@gmail.com>
2023-11-21 12:47:36 +0530

    add show more listing functionality

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -18,7 +18,7 @@ export default function Search() {
 
     const [loading, setLoading] =useState(false);
     const [listings, setListings]=useState([]);
-    console.log(listings);
+    const [showMore, setShowMore] =useState(false);
 
     useEffect(()=>{
 
@@ -45,12 +45,19 @@ export default function Search() {
                 }
                   const fetchListings =async()=>{
                     setLoading(true);
+                    setShowMore(false);
                     const searchQuery= urlParams.toString();
                     const res=await fetch(`api/listing/get?${searchQuery}`);
                     const date =await res.json();
+                    if (data.length>8) {
+                      setShowMore(true);
+                    }else{
+                      setShowMore(false);
+                    }
                     setListings(data);
                     setLoading(false);
-                  }
+                  };
+                  fetchListings();
 
             },[location.search]);
         
@@ -91,7 +98,22 @@ export default function Search() {
         const searchQuery =urlParams.toString()
         navigate(`/search? ${searchQuery}`);
         
+    };
+    
+    const onShowMoreClick =async()=>{
+      const numberOfListings =listings.length;
+      const startIndex =numberOfListings;
+      const urlParams =new URLSearchParams(location.search);
+      urlParams.set('startIndex', startIndex);
+      const searchQuery =urlParams.toString();
+      const res=await fetch(`/api/listing/get?${searchQuery}`);
+      const data= await res.json();
+      if (data.length <9) {
+        setShowMore(false);
+      }
+      setListings([...listings,...data]);
     }
+
   return (
     <div className='flex flex-col md:flex-row'>
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -190,6 +212,10 @@ export default function Search() {
                   <ListingItem key={listing._id} listing={listing}/>
                 })
               }
+              {showMore &&(
+                <button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center'>Show More</button>
+
+              )}
             </div>
       </div>
     </div>
